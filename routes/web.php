3c22de7891a8db96d6f3f299af392d0427a8a53f
Lauri Mäkinen <lauri@mits.fi>
2017-11-04 10:30:10 +0200

    A user can view all pages

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get("/pages", "PagesController@index");
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
---
+++ tests/Feature/PagesTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class PagesTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_can_browse_a_list_of_pages()
+    {
+        $page = factory("App\Page")->create();
+
+        $response = $this->get('/pages');
+
+        $response->assertStatus(200);
+        $response->assertSee($page->title);
+    }
+}
